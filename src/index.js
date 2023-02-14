1c16ea4faa4c89642d021f03d7d6a4f4e4c3ba37
gabrielpessoa97@gmail.com <gabrielpessoa97@gmail.com>
2023-02-13 22:03:07 -0300

    Feat: dotenv config

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import express, { json } from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 
 app.use(json());
 
 
-app.listen(5000, () => console.log('rodou'));
+app.listen(process.env.PORT, () => console.log(`Server is running on port ${process.env.PORT}...`));
