066d3872e47f6f80150cd468f1202967ff1a0334
gabrielpessoa97@gmail.com <gabrielpessoa97@gmail.com>
2023-02-16 17:22:21 -0300

    Feat: authentication middleware

---
+++ src/middlewares/authMiddleware.js
@@ -0,0 +1,20 @@
+import jwt from "jsonwebtoken";
+import dotenv from 'dotenv';
+dotenv.config();
+
+import db from "../database/db.js";
+
+export default async function auth(req, res, next){
+    const token = req.headers.authorization?.replace('Bearer ', '');
+    
+    try{
+       const sessionId = jwt.verify(token, process.env.JWT_SECRET);
+       const {rows} = await db.query('SELECT * FROM sessions WHERE id=$1;', [sessionId]);
+       const {userId} = rows[0];
+       res.locals.userId = userId;
+
+    }catch(err){
+        return res.status(401).send(err.message);
+    }
+    next();
+}
