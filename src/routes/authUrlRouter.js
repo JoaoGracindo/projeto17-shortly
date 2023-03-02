0473c3f27657bf6f719e45d3422f9e67043e5a86
gabrielpessoa97@gmail.com <gabrielpessoa97@gmail.com>
2023-03-02 18:16:07 -0300

    Fix: forgotten controller

--- src/routes/authUrlRouter.js
+++ src/routes/authUrlRouter.js
@@ -2,12 +2,12 @@ import { Router } from "express";
 
 import authentication from '../middlewares/authMiddleware.js';
 import {postUrlMiddleware, deleteUrlMiddleware} from '../middlewares/urlMiddlewares.js';
-import { postUrlController, deleteUrlController } from "../controllers/authUrlControllers.js";
+import { postUrlController, deleteUrlController, getMyUrlsController } from "../controllers/authUrlControllers.js";
 
 const router = Router();
 
 router.use(authentication);
-router.get('/users/me');
+router.get('/users/me', getMyUrlsController);
 router.post('/urls/shorten', postUrlMiddleware, postUrlController);
 router.delete('/urls/:id', deleteUrlMiddleware, deleteUrlController);
 
