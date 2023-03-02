cabdbe32ffcef00fbeacb57c48e0fe7451721be2
gabrielpessoa97@gmail.com <gabrielpessoa97@gmail.com>
2023-03-02 08:46:14 -0300

    Feat: url without auth router

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import userRouter from './routes/usersRouter.js';
+import authUrlRouter from './routes/authUrlRouter.js';
 import urlRouter from './routes/urlRouter.js';
 
 const app = express();
@@ -12,7 +13,7 @@ app.use(json());
 app.use(cors());
 
 app.use(userRouter);
+app.use(authUrlRouter);
 app.use(urlRouter);
-
 
 app.listen(process.env.PORT, () => console.log(`Server is running on port ${process.env.PORT}...`));
--- src/routes/urlRouter.js
+++ src/routes/urlRouter.js
@@ -1,14 +1,10 @@
 import { Router } from "express";
 
-import authentication from '../middlewares/authMiddleware.js';
-import {postUrlMiddleware, deleteUrlMiddleware} from '../middlewares/urlMiddlewares.js';
-import { postUrlController, deleteUrlController } from "../controllers/authUrlControllers.js";
-
 const router = Router();
 
-router.use(authentication);
-router.get('/users/me');
-router.post('/urls/shorten', postUrlMiddleware, postUrlController);
-router.delete('/urls/:id', deleteUrlMiddleware, deleteUrlController);
+router.get('/urls/:id');
+router.get('/urls/open/:shortUrl');
+router.get('/ranking');
+
 
 export default router;
