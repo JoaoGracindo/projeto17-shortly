05e6b60e58d799ff541e8e1ea7b0574a8e38d277
gabrielpessoa97@gmail.com <gabrielpessoa97@gmail.com>
2023-03-02 17:38:37 -0300

    Fix: routers order

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ app.use(json());
 app.use(cors());
 
 app.use(userRouter);
-app.use(authUrlRouter);
 app.use(urlRouter);
+app.use(authUrlRouter);
 
 app.listen(process.env.PORT, () => console.log(`Server is running on port ${process.env.PORT}...`));
