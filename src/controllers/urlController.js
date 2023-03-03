ba217aacbbc3ae2bbf488b8e9cf4560f7e6f8367
gabrielpessoa97@gmail.com <gabrielpessoa97@gmail.com>
2023-03-03 09:45:13 -0300

    Fix: ranking

--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -32,12 +32,12 @@ export async function redirectController(req, res){
 export async function rankingController(req, res){
 
     try{
-        const {rows} = await db.query('SELECT u.id, u.name, COUNT(l.id) AS "linksCount", SUM(l."visitCount") AS "visitCount" FROM users u JOIN urls l ON u.id=l."userId" GROUP BY u.id ORDER BY "visitCount" LIMIT 10;');
-        const names = [];
-        for(let i = 0; i < rows.length; i++){
-            names.push(rows[i].name);
-        }
-        return res.status(200).send(names);
+        const {rows} = await db.query('SELECT u.id, u.name, COUNT(l.id) AS "linksCount", SUM(l."visitCount") AS "visitCount" FROM users u LEFT JOIN urls l ON u.id=l."userId" GROUP BY u.id ORDER BY "visitCount" LIMIT 10;');
+        // const names = [];
+        // for(let i = 0; i < rows.length; i++){
+        //     names.push(rows[i].name);
+        // }
+        return res.status(200).send(rows);
 
     }catch(err){
         return res.status(500).send(err.message);
